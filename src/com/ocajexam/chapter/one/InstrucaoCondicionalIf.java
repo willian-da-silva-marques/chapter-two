c84b0a114a9bc8bbbe4a87e682f71a4ec4e84841
Willian Marques <willian.marques@compasso.com.br>
2020-01-07 23:13:09 -0300

    correção da condicional if e add da condicional if-then/if else if

---
+++ src/com/ocajexam/chapter/one/InstrucaoCondicionalIf.java
@@ -0,0 +1,25 @@
+package com.ocajexam.chapter.one;
+
+/**
+ * @since 07/01/2020
+ * @author willian 
+ * 
+ * A instrução if existe para executar condicionalmente uma
+ *         instrução, ou selecionar condicionalmente uma entre várias instruções
+ */
+public class InstrucaoCondicionalIf {
+
+	public static void main(String[] args) {
+		/**
+		 * A forma geral da instrução condicional if é:
+		 */
+		boolean expressao = true;
+		String instrucaoA = "'expressao' = true";
+		String instrucaoB = "'expressao' = false";
+		if (expressao) {
+			System.out.printf("%s", instrucaoA);
+		} else {
+			System.out.printf("%s", instrucaoB);
+		}
+	}
+}
---
+++ src/com/ocajexam/chapter/one/InstrucaoCondicionalIfThen.java
@@ -0,0 +1,37 @@
+package com.ocajexam.chapter.one;
+
+/**
+ * @since 07/01/2020
+ * @author willian 
+ * 
+ * A instrução if-then ou if else if como também é conhecida é usada quando várias 
+ * 	condições precisam ser percorridas em um cenário baseado em decisões.
+ */
+
+public class InstrucaoCondicionalIfThen {
+
+	public static void main(String[] args) {
+		/**
+		 * A forma geral da instrução condicional if-then é:
+		 */
+		boolean expressaoA = false;
+		boolean expressaoB = true;
+		String instrucaoA = "'expressaoA' = true";
+		String instrucaoB = "'expressaoB' = true";
+		if (expressaoA) {
+			System.out.printf("%s%n", instrucaoA);
+		} else if (expressaoB) {
+			System.out.printf("%s%n", instrucaoB);
+		}
+		
+		//Exemplo 1
+		int pyramidSinker = 3;
+		System.out.print("A pyramid sinker that weighs " + pyramidSinker + " ounces is ");
+		if (pyramidSinker == 2)
+			System.out.print("used for a slow moving tide.");
+		else if (pyramidSinker == 3)
+			System.out.print("used for a moderate moving tide.");
+		else if (pyramidSinker == 4)
+			System.out.print("used for a fast moving tide.");
+	}
+}
